62fdfe5f534ab3eccf6cfe8a39cac58b3b3d7822
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2022-09-18 17:09:40 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new(new EfProductDal());
+
+            foreach (var item in productManager.GetByUnitPrice(40,100))
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
